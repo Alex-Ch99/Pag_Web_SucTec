19f64ee974663611ed741ec6d7e611ed04da6518
alex chilquillo quispe <alex.chilquillo.q@gmail.com>
2023-06-27 13:02:30 -0500

    se crearon los archivos html, se comenzó con el diseño de la pagina contacto

---
+++ js/contacto.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById("button");
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Enviando...";
+
+  const serviceID = "default_service";
+  const templateID = "template_p0j46bb";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Mensaje enviado!");
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
