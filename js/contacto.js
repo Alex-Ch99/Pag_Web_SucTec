1cc3e6b267d7f04075fddecc5e40792791a97710
alex chilquillo quispe <alex.chilquillo.q@gmail.com>
2023-06-28 12:42:29 -0500

    se terminó con la pagina de contacto

--- js/contacto.js
+++ js/contacto.js
@@ -1,7 +1,11 @@
 const btn = document.getElementById("button");
+const mensajeEnviado = document.getElementById("mensaje-enviado");
 
 document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
+
+  // Guardar el texto original del botón
+  const btnTextOriginal = btn.value;
 
   btn.value = "Enviando...";
 
@@ -10,8 +14,26 @@ document.getElementById("form").addEventListener("submit", function (event) {
 
   emailjs.sendForm(serviceID, templateID, this).then(
     () => {
-      btn.value = "Send Email";
-      alert("Mensaje enviado!");
+      btn.value = "Enviar Mensaje";
+
+      // Mostrar el mensaje de "Mensaje enviado"
+      mensajeEnviado.style.display = "block";
+
+      // Después de 3 segundos, ocultar el mensaje de nuevo y limpiar el formulario
+      setTimeout(function () {
+        mensajeEnviado.style.display = "none";
+
+        // Limpieza manual del formulario
+        const form = document.getElementById("form");
+        const inputs = form.querySelectorAll("input, textarea");
+
+        inputs.forEach((input) => {
+          input.value = "";
+        });
+
+        // Restaurar el texto original del botón
+        btn.value = btnTextOriginal;
+      }, 3000);
     },
     (err) => {
       btn.value = "Enviar Mensaje";
